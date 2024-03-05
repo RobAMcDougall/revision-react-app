ef71a67580972b08fd4e556e00353c3449e202fd
90 <hi@90.gripe>
2024-03-05 15:36:03 +0000

    Add account model unit tests

--- backend/model/account.js
+++ backend/model/account.js
@@ -1,13 +1,13 @@
 const db = require("../db/db");
 
-async function getAccountById(id) {
-    const response = await db.query("SELECT * FROM account WHERE id = $1;", [id]);
-    
-    try {
-        return response.rows[0];
-    } catch {
-        throw new Error("User could not be found.");
-    }
+async function createAccount(data) {
+    const {username, email, password} = data;
+    const response = await db.query(
+        "INSERT INTO account (username, email, password) VALUES ($1, $2, $3) RETURNING *;",
+        [username, email, password]
+    );
+
+    return response.rows[0];
 }
 
 async function getAccount(username) {
@@ -20,14 +20,4 @@ async function getAccount(username) {
     }
 }
 
-async function createAccount(data) {
-    const {username, email, password} = data;
-    const response = await db.query(
-        "INSERT INTO account (username, email, password) VALUES ($1, $2, $3) RETURNING id;", 
-        [username, email, password]
-    );
-    
-    return getAccountById(response.rows[0].id);
-}
-
-module.exports = {getAccount, createAccount};
+module.exports = {createAccount, getAccount};
