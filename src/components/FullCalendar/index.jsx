d39346f551f5c8861919712a22dce5f6926c08c3
nishan <nishandudakia1@gmail.com>
2024-03-05 16:00:10 +0000

    add components

--- src/components/FullCalendar/index.jsx
+++ src/components/FullCalendar/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
 import "../../App.css";
 import { useState } from "react";
 
--- src/components/Quotes/index.jsx
+++ src/components/Quotes/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import '../../App.css'
+import React, { useState, useEffect } from "react";
+import "../../App.css";
 export default function Quotes() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(`${data.content} - ${data.author}`);
+      })
+      .catch((error) => console.error("Error fetching quote:", error));
+  }, []);
+
   return (
-    <div className='quotes-tile'><div className='quotes-tile-white'>Quotes
-      </div></div>
-  )
+    <div className="quotes-tile">
+      <div className="quotes-tile-white">
+        <div className="motivational-quote">
+          <h2>Motivational Quote</h2>
+          <blockquote>{quote}</blockquote>
+        </div>
+      </div>
+    </div>
+  );
 }
