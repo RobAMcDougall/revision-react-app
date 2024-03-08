68846834962ba8c43281d7a68d2b50be5940c3ac
malak <malookaabdalla@gmail.com>
2024-03-08 11:03:14 +0000

    hero page

--- src/components/Notes/index.jsx
+++ src/components/Notes/index.jsx
@@ -34,7 +34,7 @@ const Notes = ({ currentTimestamp, setCurrentTimestamp, playerRef }) => {
     const seconds = Math.floor(timestamp % 60);
     const formattedMinutes = String(minutes).padStart(2, "0");
     const formattedSeconds = String(seconds).padStart(2, "0");
-    return `(${formattedMinutes}:${formattedSeconds})`;
+    return `${formattedMinutes}:${formattedSeconds}`;
   };
 
   const handleSave = () => {
